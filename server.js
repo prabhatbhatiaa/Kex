fa967bc8f356b11c8b733a1b391594618294fd33
prabhatbhatiaa <prabhatbhatia09@gmail.com>
2026-08-18 22:34:34 +0530

    Added /api/v1/algorithms endpoint as GET request

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', (req,res) => {
     });
 });
 
+// API algos discovery endpoint
+app.get("/api/v1/algorithms", (req,res) => {
+    res.json({
+        hashing: ["SHA-256", "SHA-512"],
+        encryption: ["AES-256-GCM", "XOR"],
+        authentication: ["HMAC-SHA256"],
+        encoding: ["Base64"]
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Kex API running on port ${PORT}`)
 })
